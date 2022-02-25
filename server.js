a4222a7155a74d91c7a2754975b3bb0a1975fd7e
Luis Neves <luigio_nieves@yahoo.com>
2022-02-25 18:26:48 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.use((req, res, next) => {
+    console.log(new Date(Date.now()).toUTCString() + ' - ' + req.method + " request to " + req.url);
+    next();
+});
+
+app.get('/', (req,res) => {
+    let response = {
+        "message" : "OK",
+        "code" : 0
+    }
+    res.setHeader('Content-Type','application/json');
+    res.status(200).send(JSON.stringify(response));
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`);
+});
